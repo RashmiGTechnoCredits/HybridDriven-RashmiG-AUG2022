4fdf434695deb881bbd2774b13569986389c9dcf
Rashmi Ganachari <rashmiiganachari282@gmail.com>
2022-11-10 23:21:01 +0530

    Project initial setup

---
+++ src/testscripts/LoginTest.java
@@ -0,0 +1,33 @@
+package testscripts;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.PredefinedActions;
+import pages.LoginPage;
+
+public class LoginTest {
+	
+	
+	@Test
+	public void tc1() {
+		System.out.println("STEP - Launch Chrome Browser and Hit url");
+		PredefinedActions.start("https://rganachari-trials77.orangehrmlive.com/auth/seamlessLogin");
+			
+		System.out.println("STEP - Enter valid login credentials");
+		LoginPage loginPage = new LoginPage();
+		loginPage.login("Admin","5ZXpcAy1Q@");
+		
+		System.out.println("VERIFY -  home page is displayed");
+		String expectedTitle = "Employee Management";
+		String actualTitle = loginPage.getPageTitle();
+		
+		Assert.assertEquals(actualTitle, expectedTitle, "Expected title was " + expectedTitle + " but actual title was " + actualTitle);
+		
+		PredefinedActions.closeBrowser();
+
+	}
+}
